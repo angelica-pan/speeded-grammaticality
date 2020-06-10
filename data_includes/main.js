69a1ebe427a560d1277243475465ff6bc7914d10
Angelica <angelicapan@g.ucla.edu>
2020-06-10 13:09:30 -0700

    main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -4,35 +4,21 @@ var showProgressBar = false;                            // Don't show progress b
 
 ////////////////////////////////////////////////////////////////////////////////
 
-Sequence("intro", "trial", "send")
+
 ////////////////////////////////////////////////////////////////////////////////
 
-customButton = text  => 
-    newButton(text)
-        .center()
-        .css({"background-color":"lightblue"})
-        .print()
-        .wait()
-        
-// Post-practice items
-newTrial("intro",
-    newText("done", "sample text")
-        .center()
-        .bold()
-        .css({"width": "300px"})
-    ,
-    newCanvas(300, 190)
-        .add(0, 150, getText("done"))
-        .print()
-    ,
-    customButton("Click here to start the experiment")
-)
-
-newController("trial", "DashedSentence", {s:"this is a test", "mode":"speeded acceptability", "display":"in place", "wordTime":200})
+newButton("Start reading")
+    .print()
+    .wait()
+    .remove()
+,
+newController("DashedSentence", {s:"this is a test", "mode":"speeded acceptability", "display":"in place", "wordTime":200})
     .print()
     .log()
     .wait()
     .remove()
-
+,
+newText("Good job!")
+    .print()
 // Send results
-PennController.SendResults("send");
+// PennController.SendResults("send");
