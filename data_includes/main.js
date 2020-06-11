793ff3ded08b96179612cae5460054cce0e8c3ec
Angelica <angelicapan@g.ucla.edu>
2020-06-11 13:42:36 -0700

    main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -2,23 +2,25 @@ PennController.ResetPrefix(null);                       // Initiates PennControl
 // PennController.DebugOff()
 var showProgressBar = false;                            // Don't show progress bar
 
-////////////////////////////////////////////////////////////////////////////////
-
 
 ////////////////////////////////////////////////////////////////////////////////
 
-newButton("Start reading")
-    .print()
-    .wait()
-    .remove()
-//,
-// newController("DashedSentence", {s:"this is a test", "mode":"speeded acceptability", "display":"in place", "wordTime":200})
-//     .print()
-//     .log()
-//     .wait()
-//     .remove()
-,
-newText("Good job!")
-    .print()
+
+////////////////////////////////////////////////////////////////////////////////
+newTrial("trial",
+    newButton("Start reading")
+        .print()
+        .wait()
+        .remove()
+    ,
+    newController("DashedSentence", {s:"this is a test", "mode":"speeded acceptability", "display":"in place", "wordTime":200})
+        .print()
+        .log()
+        .wait()
+        .remove()
+    ,
+    newText("Good job!")
+        .print()
+)
 // Send results
 // PennController.SendResults("send");
