0351ca1c2798e466cb411462be3091f6a9975d30
Angelica <angelicapan@g.ucla.edu>
2020-06-11 14:40:23 -0700

    main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -8,31 +8,51 @@ var showProgressBar = false;                            // Don't show progress b
 
 ////////////////////////////////////////////////////////////////////////////////
 AddTable( "mytable" ,
-    "Type,Condition,Sentence\n"+
-    "Test,A,The cat that is chasing the mouse runs fast\n"+
-    "Test,B,The mouse that the cat is chasing runs fast"
+    "exp,item,sentence\n"+
+    "test,1,According to the story Mary was found by John, and then Peter was too.\n"+
+    "test,2,Another test sentence\n"+
+    "vpe,1,The cat that is chasing the mouse runs fast\n"+
+    "vpe,2,The mouse that the cat is chasing runs fast"
 )
 
 Template("mytable" , variable => 
     newTrial(
+        defaultText
+            .center()
+        ,
         newButton("Start reading")
             .print()
             .wait()
             .remove()
         ,
-        newController("DashedSentence", {s:variable.Sentence, "mode":"speeded acceptability", "display":"in place", "wordTime":200})
+        newController("DashedSentence", {s:variable.sentence, "mode":"speeded acceptability", "display":"in place", "wordTime":200})
             .print()
             .log()
             .wait()
             .remove()
         ,
-        newButton("Next")
+        newText("judgment", "text here")
+        .cssContainer({"width": "600px", "font-size": "150%", "height": "50px"})
+        ,
+        // F for not okay, J for okay
+        newText("F", "press F for not okay")
+            .italic()
+            .cssContainer({"width": "300px"})
+        ,
+        newText("J", "press J for okay")
+            .italic()
+            .cssContainer({"width": "300px"})
+        ,
+        newCanvas("q_display", 600, 300)
+            .add(0, 145, getText("judgment"))
+            .add(0, 200, getText("F"))
+            .add(300, 200, getText("J"))
             .print()
-            .wait()
-            .remove()
+            .log()
     )
 )
-
+.log("exp", variable.group)
+.log("item", variable.group)
 
 // Send results
 // PennController.SendResults("send");
