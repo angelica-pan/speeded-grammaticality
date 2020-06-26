cb9d2dfad5e2a4ff66e4039174c0231b8a0c106b
Angelica <angelicapan@g.ucla.edu>
2020-06-26 15:04:22 -0700

    version 0

--- data_includes/main.js
+++ data_includes/main.js
@@ -6,11 +6,13 @@
 // 2. Stimulus RSVP 
 // 3a. Judgment (participant presses F or J to indicate judgment)
 // 3b. Time out (skips to next section/trial if participant does not provide judgment within time frame)
+		// Version 0: no time out (participants have as much time as they want to provide judgment)
+			// Comment out lines 193-205
 		// Version 1: ["tooSlow"] message prints before next section begins 
-			// Comment out lines 200-201
+			// Comment out lines 201-202
 			// i.e. print comprehension question if there is one
 		// Version 2: next trial begins immediately 
-			// Comment out lines 197-200
+			// Comment out lines 198-201
 			// i.e. skip comprehension question even if there is one
 		// Version 3: ["tooSlow"] message prints before next trial begins 
 			// Don't comment out anything
@@ -20,8 +22,7 @@
 // 6. Break trial (participant has opportunity to take longer break and sees comprehension question accuracy)
 //
 // Sections 4 and 5 only display when the source CSV has non-empty [question] and [feedback] values
-//
-// Remove section 3b (participants have as much time as they want to provide judgment) by commenting out lines
+
 
 // CSV must have the following column(s): [sentence]
 // CSV should have the following column(s): [question], [F_answer], [J_answer], [feedback] 
@@ -189,19 +190,19 @@ customTrial = label => variable => newTrial( label ,
     	.cssContainer({"margin":"105px 0 0 0", "width":"600px"})
     ,
     clear()
-    ,
-	getKey("judgment")
-    	.test.pressed()
-    	.success(newText(" ").print())
-    	.failure(
-    		getText("tooSlow").print(),
-    		getText("next").print(),
-    		getKey("continue").wait()
+//     ,
+// 	getKey("judgment")
+//     	.test.pressed()
+//     	.success(newText(" ").print())
+//     	.failure(
+//     		getText("tooSlow").print(),
+//     		getText("next").print(),
+//     		getKey("continue").wait()
 //     		,			
 //     		end()
-    		)
-	,
-	clear()
+//     		)
+// 	,
+// 	clear()
 	,
 	// 4. Comprehension question 
     (variable.question?[
